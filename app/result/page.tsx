25019cacadb951ed47b5ac33d3002131437a54b4
Ben Hur <bhur777@gmail.com>
2023-10-04 03:04:20 -0400

    Reset answer and questions and then retry

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -3,11 +3,15 @@ import { useQnIdStore } from "@/hooks/useIdStore";
 import { useEffect, useState } from "react";
 import { ENDPOINTS, createAPIEndpoint } from "../api";
 import { useAnswerStore } from "@/hooks/useAnswerStore";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 
 const ResultPage = () => {
+	const router = useRouter();
 	const qnIds = useQnIdStore((state) => state.qnIds);
 	const answers = useAnswerStore((state) => state.answers);
+	const { setContextQnId } = useQnIdStore();
+	const { setContextAnswers } = useAnswerStore();
 	const [score, setScore] = useState(0);
 
 	const mapQNA = () => {
@@ -41,7 +45,11 @@ const ResultPage = () => {
 		setScore(correct);
 	};
 
-	const retry = () => {};
+	const retry = () => {
+		setContextQnId([]);
+		setContextAnswers([]);
+		router.push("/quiz");
+	};
 
 	return (
 		<div className="flex justify-center items-center h-full">
@@ -57,7 +65,7 @@ const ResultPage = () => {
 					Results: {score} out of 5 correct!
 				</div>
 				<div className="text-white text-2xl mt-2">Want to try again?</div>
-				<button onClick={} className="bg-white mt-6 px-8 py-2 rounded-sm">
+				<button onClick={retry} className="bg-white mt-6 px-8 py-2 rounded-sm">
 					Retry
 				</button>
 			</div>
